f478d53173555a9e17bf6d4237d37b62ab605e5b
William R. Ferreira <willian.usp@hotmail.com>
2021-04-23 17:28:45 -0300

    Implementação de 'server' em NodeJs. Criação da estrutura de pastas e arquivos para organização do server

---
+++ bin/server.js
@@ -0,0 +1,59 @@
+const app = require('../src/app')
+const debug = require('debug')('EngTuran:server')
+const http = require('http')
+
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port',port)
+
+const server = http.createServer(app)
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+console.log("API rodando na porta " + port)
+//console.log(normalizePort(process.env.PORT || '3000'))
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+}
+
+function onError(error){
+    if(error.syscall !== 'listen'){
+        throw error
+    }
+
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
+
+    switch(error.code) {
+        case 'EACCES':
+            console.log(bind + ' requires elevated privileges ')
+            process.exit(1)
+            break
+        case 'EADDRINUSE':
+            console.log(bind + ' is already in use ')
+            process.exit(1)
+            break
+        default:
+            throw error
+    }
+}
+
+function onListening(){
+    const addr = server.address()
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr 
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind)
+}
