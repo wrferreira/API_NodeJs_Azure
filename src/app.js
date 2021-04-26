e5defcfe64a010493b856e6199234aecf34b07cf
William R. Ferreira <willian.usp@hotmail.com>
2021-04-26 17:23:00 -0300

    Implementação da conexão com MongoDB e configuração do Schema 'Products'

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
 'use strict'
 
 const express = require('express')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+//const bodyParser = require('body-parser') // ==> deprecated
 
 const app = express()
 const router = express.Router()
+
+//Conecta ao Banco
+mongoose.connect('mongodb+srv://engturan:macaco22@cluster0.gueuj.mongodb.net/node-store')
+
+//Carregando os Models
+const Product = require('./models/product')
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index')
 const productRoute = require('./routes/product')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json()) // ==> bodyparse: deprecated 
+app.use(express.urlencoded({ extended: false })) // ==> bodyparse: deprecated
 
 
 
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,20 @@
 'use strict'
 
+const mongoose = require('mongoose')
+const Product = require('../models/product')
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body)
+    var product = new Product(req.body)
+    product
+        .save()
+        .then( ret => {
+            console.log(ret)
+            res.status(200).send({ message: 'Produto cadastrado com sucesso!'})
+        })
+        .catch( e => {
+            console.log(e)
+            res.status(400).send({ message: 'Falha ao cadastro o produto', data: e})
+        })
 }
 
 exports.put = (req, res, next) => {
